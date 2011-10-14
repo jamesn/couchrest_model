eceb4925c9c88810733d6be86e7e4f6ec25aa247
James Nuckolls <jamesn@softlayer.com>
2011-10-14 11:50:03 -0500

    convert the object to a hash, then dup it to avoid smashing @_attributes

--- lib/couchrest/model/support/couchrest_design.rb
+++ lib/couchrest/model/support/couchrest_design.rb
@@ -8,26 +8,26 @@ CouchRest::Design.class_eval do
   # design document, convert to string then generate an MD5 Hash. This should
   # result in a consisitent Hash accross all platforms.
   #
+
   def checksum!
     # create a copy of basic elements
-    base = self.dup
+    base = self.to_hash.dup
     base.delete('_id')
     base.delete('_rev')
     base.delete('couchrest-hash')
     result = nil
     flatten =
-      lambda {|r|
-        (recurse = lambda {|v|
-          if v.is_a?(Hash) || v.is_a?(CouchRest::Document)
-            v.to_a.map{|v| recurse.call(v)}.flatten
-          elsif v.is_a?(Array)
-            v.flatten.map{|v| recurse.call(v)}
-          else
-            v.to_s
-          end
+    lambda {|r|
+      (recurse = lambda {|v|
+        if v.is_a?(Hash) || v.is_a?(CouchRest::Document)
+          v.to_a.map{|v| recurse.call(v)}.flatten
+        elsif v.is_a?(Array)
+          v.flatten.map{|v| recurse.call(v)}
+        else
+          v.to_s
+        end
         }).call(r)
       }
-    self['couchrest-hash'] = Digest::MD5.hexdigest(flatten.call(base).sort.join(''))
+      self['couchrest-hash'] = Digest::MD5.hexdigest(flatten.call(base).sort.join(''))
+    end
   end
-
-end
